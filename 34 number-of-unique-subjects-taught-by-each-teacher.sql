1e3fc465d20c3f5232dc7a7feda7e6c4f7be62b2
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2025-05-30 19:10:41 +0530

    Create 34 number-of-unique-subjects-taught-by-each-teacher.sql

---
+++ 34 number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,14 @@
+/*Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ */
+
+SELECT teacher_id,COUNT(DISTINCT Subject_id) as cnt FROM Teacher GROUP BY teacher_id;
